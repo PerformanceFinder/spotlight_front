3e4a3e6661f48705be1bdad1e9b5e7e615a25570
immanuelk1m <kse0119@naver.com>
2024-08-13 18:43:18 +0900

    "init"

---
+++ spotlight/app/page.js
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <div className="text-center">
+        <div className="mb-8 flex justify-center">
+          <Image
+            src="/logo.png"
+            alt="Artause Logo"
+            width={256}
+            height={64}
+            priority
+          />
+        </div>        
+        <p className="mb-6 text-lg text-gray-700">
+          그동안 몰랐던, 하지만 나에게 딱 맞는<br />
+          공연을 추천해드려요!
+        </p>
+        <Button
+          variant="default"
+          className="p-0 overflow-hidden"
+        >
+          <Image
+            src="/kakao_login_medium_narrow.png"
+            alt="카카오 로그인"
+            width={183}
+            height={45}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
