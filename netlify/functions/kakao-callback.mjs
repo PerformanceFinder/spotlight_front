d2358417aedf3b4bbd8f7446f5b95682dadbecc8
immanuelk1m <kse0119@naver.com>
2024-08-17 17:01:06 +0900

    refix

--- netlify/functions/kakao-callback.mjs
+++ netlify/functions/kakao-callback.mjs
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   const { code } = event.queryStringParameters;
 
   if (!code) {
