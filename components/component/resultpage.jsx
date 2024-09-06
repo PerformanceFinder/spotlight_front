261bb963e778d25ad3e116244cb75d2d6a9bad87
immanuelk1m <kse0119@naver.com>
2024-09-06 22:48:48 +0900

    fix again

--- components/component/resultpage.jsx
+++ components/component/resultpage.jsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export function ResultPage() {
+function SearchResult() {
   const searchParams = useSearchParams();
   const keyword = searchParams.get('keyword');  // 쿼리 파라미터에서 'keyword' 값 가져오기
 
@@ -11,5 +12,13 @@ export function ResultPage() {
       <h1>Search Page</h1>
       <p>Search Keyword: {keyword}</p>
     </div>
+  );
+}
+
+export default function ResultPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchResult />
+    </Suspense>
   );
 }
