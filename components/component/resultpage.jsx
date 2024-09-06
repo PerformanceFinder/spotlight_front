2fefdfc2a892be8f003bee180d84c48d34bbf1a6
immanuelk1m <kse0119@naver.com>
2024-09-06 23:12:42 +0900

    fix it

--- components/component/resultpage.jsx
+++ components/component/resultpage.jsx
@@ -1,21 +1,39 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation';
+import { useEffect } from "react";
 import { Suspense } from 'react';
 
 function SearchResult() {
   const searchParams = useSearchParams();
-  const keyword = searchParams.get('plays');  // 쿼리 파라미터에서 'keyword' 값 가져오기
+  const keyword = searchParams.get('plays');
+
+  const fetchPlays = async () => {
+    try {
+      const response = await fetch(`https://artause.co.kr/userselect?plays=${plays}`);
+      const data = await response.json();
+      
+    } catch (error) {
+      console.error("Error fetching plays:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlays();
+  }, []);
 
   return (
     <div>
       <h1>Search Page</h1>
-      <p>Search Keyword: {keyword}</p>
+      <p>Search Keyword: {data}</p>
     </div>
   );
 }
 
 export function ResultPage() {
+
+  
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <SearchResult />
