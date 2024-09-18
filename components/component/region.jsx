698e604782cd002931e8c3f12818a57e9a1accc6
immanuelk1m <kse0119@naver.com>
2024-09-18 13:27:44 +0900

    use navigation

--- components/component/region.jsx
+++ components/component/region.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, createContext, useContext } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 
 const regions = {
